82a077e5cacaef9d44bf2d14186e5c1c2e6835c4
ankit-tally <117451786+ankit-tally@users.noreply.github.com>
2022-12-30 14:53:55 +0530

    Update and rename main.js to firebase.js
    added the function to receive foreground message.
    added the function to only generate token if service Worker is registered for the browser.

---
+++ src/firebase.js
@@ -0,0 +1,98 @@
+import { initializeApp } from 'firebase/app';
+import { getToken, getMessaging, onMessage, deleteToken } from 'firebase/messaging';
+import { getDatabase, ref, set, remove, query} from "firebase/database"
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrHXIF6tg-3u142utCjmy0IfK3SH-xJkc",
+    authDomain: "tally-notitia.firebaseapp.com",
+    projectId: "tally-notitia",
+    storageBucket: "tally-notitia.appspot.com",
+    messagingSenderId: "286442771525",
+    appId: "1:286442771525:web:609302b89b5ab25d89bf2c",
+  };
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+const database = getDatabase();
+
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export const signIn = () => {
+  signInWithPopup(auth, provider)
+    .then(() => {
+      console.log("Hello (:-");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message);
+    });
+}
+
+export const signOff = () => {
+  signOut(auth)
+  .then(() => {
+    console.log("Successfully Signed Out!")
+  })
+  .catch(() => {
+  });
+}
+
+export const getOrRegisterServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    return window.navigator.serviceWorker
+      .getRegistration('/firebase-push-notification-scope')
+      .then((serviceWorker) => {
+        if (serviceWorker) return serviceWorker;
+        return window.navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+          scope: '/firebase-push-notification-scope',
+        });
+      });
+  }
+  throw new Error('The browser doesn`t support service worker.');
+};
+
+export const subscribeButton = () => {
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      getToken(messaging, {
+        vapidKey:
+          "BBhiEzNEIZhoe0st_ngva0ADh-SEVaaUb1QiDJVqqvhdrI3eJkh76QMgKt8-cFpJk_rIG4WQ8k8fHTk2LNLi6rE",
+      }).then((currentToken) => {
+        if (currentToken) {
+          console.log("currentToken: ", currentToken);
+          set(ref(database, "/tokens/" + auth.currentUser.uid), {
+            name: auth.currentUser.displayName,
+            token: currentToken
+          });
+        } else {
+          console.log("Can not get token");
+        }
+      });
+    }
+  });
+};
+
+
+export const unsubscribeButton = () => {
+  deleteToken(messaging, {
+    vapidKey:
+      "BBhiEzNEIZhoe0st_ngva0ADh-SEVaaUb1QiDJVqqvhdrI3eJkh76QMgKt8-cFpJk_rIG4WQ8k8fHTk2LNLi6rE",
+  })
+  .then(() => {
+    const queryConstraints = query(
+      ref(database, "/tokens/" + auth.currentUser.uid)
+    );
+    remove(queryConstraints).then((snapshot) => {
+      console.log("Removed Successfully (:-");
+    });
+  })
+  .catch((err) => console.log("Error Deleting Token"));
+};
+
+export const onForegroundMessage = () =>
+new Promise((resolve) => onMessage(messaging, (payload) => resolve(payload)));
+
