d7d93736d3e5b9cc70d656085788cdd7f4c02d24
ankit-tally <117451786+ankit-tally@users.noreply.github.com>
2023-01-04 14:00:33 +0530

    Create New Page for Admin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useEffect} from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import axios from 'axios';
 
 import { onForegroundMessage, unsubscribeButton, signIn, signOff, subscribeButton} from './firebase';
 
-const baseURL = "https://fcm.googleapis.com/fcm/send";
 
 
 export default function App() {
-
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'key=AAAAQrFR1EU:APA91bEWVAggsr-qPaqFa_a-ticGg1c38xS1EuIXF3Xn7c59SnW2tYUUpW0eL_VlS201aUVhi2-DQBpgehbDjh_gcYhebgQ5_EsVZwoaaC8hbxjme5E8AE_R0_X8IQtO6WCX5FDEZ1as'
-  }
-
 
   useEffect(() => {
     onForegroundMessage()
@@ -32,30 +24,6 @@ export default function App() {
     </div>
   );
 
-  function handleClick() {
-    axios
-      .post(baseURL, {
-        "notification": {
-          "title": titleRef.current.value,
-          "body": messRef.current.value,
-          "icon": "firebase-logo.png"
-        },
-        "to": tokenRef.current.value
-      }, {
-        headers: headers
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => console.log(err));
-  }
-
-
-
-  const titleRef = useRef(null);
-  const messRef = useRef(null);
-  const tokenRef = useRef(null);
-
   
   return (
     <div className="app">
@@ -71,32 +39,6 @@ export default function App() {
       </button>
       <button className="btn-primary" onClick={unsubscribeButton}>
         Unsubscribe
-      </button>
-
-
-
-      <form>
-        <label>
-          Title:
-          <input ref={titleRef} type="text" id="title" name="title" />
-        </label>
-        <label>
-          Message:
-          <input ref={messRef} type="text" id="message" name="message" />
-        </label>
-        <label>
-          Token:
-          <input ref={tokenRef} type="text" id="token" name="token" />
-        </label>
-      </form>
-
-      <button
-        className="btn-primary"
-        onClick={() => {
-          handleClick();
-        }}
-      >
-        Send Notification
       </button>
 
       <ToastContainer
